9590a9144073c08880d0e9e1af5310ae7079c516
nkhandwe <nkhandwe@gmail.com>
2022-02-06 11:21:35 +0530

    adeded

---
+++ lib/config/config.dart
@@ -0,0 +1,13 @@
+class Config {
+  final String appName = 'General Information'; //app name
+  final String testerPassword =
+      'tester12345'; //testing password - don't use this password in the database (you can change this testing password too)
+
+  //firebase server token for push notication
+  final String serverToken =
+      '	AAAAfEjv1Qo:APA91bFN0VUnMcdid-MxDw2L8ynxVdIgF4HMY466G_hftE521whcqdp-Maqe10jNXyE5bmMrziqccsFMv2K3i7Fu9Hg0H7KluCR2IOCqSwow-HDmyuCN6rD54_kitDI6m3BPpxFz2EUa';
+  final String icon = 'assets/images/icon.png'; // app icon
+
+  //don't edit or remove this
+  final List contentTypes = ['image', 'video'];
+}
